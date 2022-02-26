2b569462823de23531a5dc6e07f0eb13e30dc06d
IakonaColobong <jason.colobong@outlook.com>
2022-02-26 13:13:45 -0600

    Metadata created

--- StoreFront.Data.EF/StoreMetadata/StoreMetadata.cs
+++ StoreFront.Data.EF/StoreMetadata/StoreMetadata.cs
@@ -11,97 +11,153 @@ namespace StoreFront.Data.EF//.StoreMetadata
     #region AuthorsTableMetadata
     public class AuthorTablesMetadata
     {
-        public int AuthorID { get; set; }
+
+        //public int AuthorID { get; set; }
+        [DisplayFormat(NullDisplayText = "[-N/A-]")]
+        [Display(Name = "First Name")]
+        [StringLength(20, ErrorMessage = "Max 20 Characters")]
         public string FName { get; set; }
+
+        [DisplayFormat(NullDisplayText = "[-N/A-]")]
+        [Display(Name = "Last Name")]
+        [StringLength(20, ErrorMessage = "Max 20 Characters")]
         public string LName { get; set; }
     }
     [MetadataType(typeof(AuthorTablesMetadata))]
-    public partial class AuthorTables { } 
+    public partial class AuthorTables
+    {
+        //custom property
+        //[Display(Name = "Full Name")]
+        //public string FullName
+        //{
+        //    get { return $"{FName} {LName}"; }
+        //}
+
+    }
+
+
     #endregion
-
-
+    
     #region BooksTableMetadata
     public class BooksTableMetadata
     {
 
-        public int BookID { get; set; }
-        public string BooksTitle { get; set; }
-        public int GenreID { get; set; }
-        public int AuthorID { get; set; }
-        public Nullable<decimal> Price { get; set; }
-        public Nullable<int> UnitsSold { get; set; }
-        public Nullable<System.DateTime> PublishDate { get; set; }
-        public Nullable<int> Publisher { get; set; }
-        public int StockID { get; set; }
-        public Nullable<int> CategoryID { get; set; }
+        //public int BookID { get; set; }
 
-        public virtual AuthorTable AuthorTable { get; set; }
-        public virtual Category Category { get; set; }
-        public virtual GenreIDTable GenreIDTable { get; set; }
-        public virtual Stock Stock { get; set; }
+        [Required(ErrorMessage = "*")]
+        [StringLength(50, ErrorMessage = "Must be 50 characters or less")]
+        public string BooksTitle { get; set; }
+
+
+        //public int GenreID { get; set; }
+
+
+        //public int AuthorID { get; set; }
+        [DisplayFormat(NullDisplayText = "[-N/A-]")]
+        [Range(0, double.MaxValue, ErrorMessage = "* Value must be a valid number, 0 or larger.")]
+      
+        public Nullable<decimal> Price { get; set; }
+
+
+        //public Nullable<int> UnitsSold { get; set; }
+      
+        [Display(Name = "Publish Date")]
+        [DisplayFormat(DataFormatString = "{0:d}", ApplyFormatInEditMode = true, NullDisplayText = "[-N/A-")]
+        public Nullable<System.DateTime> PublishDate { get; set; }
+
+
+        //public Nullable<int> Publisher { get; set; }
+
+
+        //public int StockID { get; set; }
+
+
+        //public Nullable<int> CategoryID { get; set; }
+
+        //public virtual AuthorTable AuthorTable { get; set; }
+        //public virtual Category Category { get; set; }
+        //public virtual GenreIDTable GenreIDTable { get; set; }
+        //public virtual Stock Stock { get; set; }
     }
 
     [MetadataType(typeof(BooksTableMetadata))]
     public partial class BooksTable { }
 
     #endregion
-
-
-
+       
     #region CategoryMetadata
-
-
     public class CategoryMetadata
     {
-        public int CategoryID { get; set; }
+        //public int CategoryID { get; set; }
+        [Required(ErrorMessage = "Required Entry")]
+        [StringLength(50, ErrorMessage = "Must be less than 50 characters.")]
         public string CategoryName { get; set; }
     }
     [MetadataType(typeof(CategoryMetadata))]
-    public partial class Category { } 
+    public partial class Category { }
     #endregion
-
-
+    
     #region EmployeeMetadata
     public class EmployeeMetadata
     {
-        public int EmployeeID { get; set; }
+        //public int EmployeeID { get; set; }
+        [Required(ErrorMessage = "Required Entry")]
+        [Display(Name = "First Name")]
+        [StringLength(20, ErrorMessage = "20 character maximum")]
         public string FirstName { get; set; }
+
+        [DisplayFormat(NullDisplayText = "[-N/A-]")]
+        [Display(Name = "Last Name")]
+        [Required(ErrorMessage = "Required Entry")]
+        [StringLength(20, ErrorMessage = "20 character maximum")]
         public string LastName { get; set; }
+
+        [DisplayFormat(NullDisplayText = "[-N/A-]")]
+        [Display(Name = "Employee Title")]
+        [StringLength(30, ErrorMessage = "30 character maximum")]
         public string Title { get; set; }
+
+        [Display(Name = "Hire Date")]
+        [DisplayFormat(DataFormatString = "{0:d}", ApplyFormatInEditMode = true, NullDisplayText = "[-N/A-")]
         public Nullable<System.DateTime> HireDate { get; set; }
-        public Nullable<int> DirectReportID { get; set; }
+
+        //[Display(Name = "Reportable To")]
+
+        //public Nullable<int> DirectReportID { get; set; }
 
 
     }
     [MetadataType(typeof(EmployeeMetadata))]
-    public partial class Employee { } 
+    public partial class Employee { }
     #endregion
-
-
+    
     #region GenreMetadata
     public class GenreIDMetadata
     {
-        public int GenreID { get; set; }
+        //public int GenreID { get; set; }
+        [DisplayFormat(NullDisplayText = "[-N/A-]")]
+        [Display(Name = "Genre")]
+        [StringLength(20, ErrorMessage = "20 character maximum")]
         public string GenreType { get; set; }
 
     }
 
     [MetadataType(typeof(GenreIDMetadata))]
-    public partial class GenreID { } 
+    public partial class GenreID { }
     #endregion
-
-
-
+       
     #region StockMetadata
     public class StockMetadata
     {
 
-        public int StockID { get; set; }
+        //public int StockID { get; set; }
+        [Required(ErrorMessage = "Required Field")]
+        [StringLength(20, ErrorMessage = "Maximum of 20 characters")]
         public string StockType { get; set; }
 
     }
 
     [MetadataType(typeof(StockMetadata))]
-    public partial class Stock { } 
+    public partial class Stock { }
     #endregion
 }
